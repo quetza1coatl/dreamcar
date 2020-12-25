d4cc8a6e16f976032bc08b99cd44dd64b4ca331f
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2020-12-25 18:21:31 +0300

    add LDAP for inventory system

---
+++ inventory-system/src/main/java/com/dreamcar/inventorysystem/security/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.dreamcar.inventorysystem.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/css/**").permitAll().anyRequest()
+                .fullyAuthenticated().and().formLogin();
+    }
+
+    @Configuration
+    protected static class AuthenticationConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
+
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            auth.ldapAuthentication().userDnPatterns("uid={0},ou=people")
+                    .groupSearchBase("ou=groups").contextSource()
+                    .ldif("classpath:ldap-server.ldif");
+        }
+    }
+}
