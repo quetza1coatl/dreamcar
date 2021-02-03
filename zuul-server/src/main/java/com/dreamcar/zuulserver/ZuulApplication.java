0172bd1fe50e43555e00ade51c9354b8abbc2afd
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-02-03 22:50:52 +0300

    add zuul

---
+++ zuul-server/src/main/java/com/dreamcar/zuulserver/ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.dreamcar.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class ZuulApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ZuulApplication.class);
+    }
+}
